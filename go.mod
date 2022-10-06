d89c3254cb9fdf3a0415718061fe08d9a5378def
Igor <vedmak3@list.ru>
2022-10-06 16:36:36 +0500

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module webdav
 
 go 1.19
 
-require golang.org/x/net v0.0.0-20220909164309-bea034e7d591
+require golang.org/x/net v0.0.0-20221004154528-8021a29435af
--- server.go
+++ server.go
@@ -16,46 +16,48 @@ func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 }
 func main() {
-	arg := os.Args[1:]
+	//arg := os.Args[1:]
 	rand.Seed(time.Now().UnixNano())
-	if len(arg) == 2 {
-		salt := RandStringRunes(8)
-		fmt.Println(string(salt))
-		os.Mkdir("./storage", 0777)
-		storagePath := "./storage"
+	//if len(arg) == 2 {
+	salt := RandStringRunes(8)
+	fmt.Println(string(salt))
+	os.Mkdir("./storage", 0777)
+	storagePath := "./storage"
 
-		srv := &webdav.Handler{
-			Prefix:     "/" + salt,
-			FileSystem: webdav.Dir(storagePath),
-			LockSystem: webdav.NewMemLS(),
-			Logger: func(r *http.Request, err error) {
-				if err != nil {
-					fmt.Printf("WebDAV %s: %s, ERROR: %s\n", r.Method, r.URL, err)
-				}
-			},
-		}
-
-		mux := http.NewServeMux()
-		// Trailing slash must be inputed to end of path in http.HandleFunc
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			log.Println(r)
-		})
-		mux.HandleFunc("/"+salt+"/", func(w http.ResponseWriter, r *http.Request) {
-			username, password, _ := r.BasicAuth()
-
-			// Check credential
-			if username == arg[0] && password == arg[1] {
-				// User control at here, if required.
-
-				srv.ServeHTTP(w, r)
-				return
+	srv := &webdav.Handler{
+		Prefix:     "/" + salt,
+		FileSystem: webdav.Dir(storagePath),
+		LockSystem: webdav.NewMemLS(),
+		Logger: func(r *http.Request, err error) {
+			if err != nil {
+				fmt.Printf("WebDAV %s: %s, ERROR: %s\n", r.Method, r.URL, err)
 			}
-
-			w.Header().Set("WWW-Authenticate", `Basic realm="BASIC WebDAV REALM"`)
-			w.WriteHeader(401)
-			w.Write([]byte("401 Unauthorized\n"))
-		})
-
-		tlsServer(":443", mux)
+		},
 	}
+
+	mux := http.NewServeMux()
+	// Trailing slash must be inputed to end of path in http.HandleFunc
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r)
+	})
+	mux.HandleFunc("/"+salt+"/", func(w http.ResponseWriter, r *http.Request) {
+		//username, password, _ := r.BasicAuth()
+
+		// Check credential
+		//if username == arg[0] && password == arg[1] {
+		// User control at here, if required.
+
+		srv.ServeHTTP(w, r)
+		//return
+		//}
+
+		/*w.Header().Set("WWW-Authenticate", `Basic realm="BASIC WebDAV REALM"`)
+		w.WriteHeader(401)
+		w.Write([]byte("401 Unauthorized\n"))*/
+	})
+
+	//	tlsServer(":443", mux)
+	http.ListenAndServe(":80", mux)
 }
+
+//}
